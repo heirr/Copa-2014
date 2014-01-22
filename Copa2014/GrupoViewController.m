e7e71268748217fd404854cfbdcb93509882e6be
heirr <kingclark@gmail.com>
2014-01-21 23:05:46 -0400

    alterações na tela de splash e imagens

--- Copa2014/GrupoViewController.m
+++ Copa2014/GrupoViewController.m
@@ -30,12 +30,30 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    
+    //Metodo para definir a tela de fundo para iPhone 3.5 e 4 polegadas
+    [self.view setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"imgTelaGrupos"]]];
+    NSLog(@"%f",[UIScreen mainScreen].bounds.size.height);
+    
+    //Alterar posicao de labels de acordo com tamanho da tela
+    if ([UIScreen mainScreen].bounds.size.height < 560.0f) {
+        self.lblTeste.autoresizingMask = UIViewAutoresizingNone;
+        self.lblTeste.frame = CGRectMake(10, 10, 280, 21);
+        self.lblTeste.text  = @"teste1";
+        [self.view frame];
+    }
+    else {
+        self.lblTeste.autoresizingMask = UIViewAutoresizingNone;
+        self.lblTeste.frame = CGRectMake(20, 200, 280, 21);
+        self.lblTeste.text = @"teste2";
+        [self.view frame];
+    }
+    //
     NSLog(@"teste");
     NSString *plistFile = [[NSBundle mainBundle] pathForResource:@"grupos" ofType:@"plist"];
     NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile:plistFile];
     NSLog(@"%@", dict);
     self.lblTime1.text = [[[[dict objectForKey:@"grupos"] objectForKey:@"a"] objectForKey:@"selecoes"] objectAtIndex:0];
-    self.lblTeste.text = teste;
 
 }
 
